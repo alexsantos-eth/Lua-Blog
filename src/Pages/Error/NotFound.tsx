a01296f5f79b93b9b3925d04208cb9b4d26149b5
alexsan-dev <alexdanielsantosv@gmail.com>
2020-08-08 17:31:36 -0600

    [Fix] 404 title

--- src/Pages/Error/NotFound.tsx
+++ src/Pages/Error/NotFound.tsx
@@ -3,10 +3,18 @@ import React from 'react'
 
 // ESTILOS
 import Styles from './NotFound.module.scss'
+import Meta from 'Components/Meta/Meta'
 
 const NotFound: React.FC = () => {
 	return (
 		<section className={Styles.page}>
+			<Meta 
+				title='Error 404 página no encontrada' 
+				banner='' 
+				url=''
+			 	desc='Error 404 página no encontrada' 
+			 	keys={['LUA', 'Blog']}
+			/>
 			<h1>Error 404 página no encontrada</h1>
 		</section>
 	)
